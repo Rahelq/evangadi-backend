79bba1b2b4a396e01655dfdbbffeb61bb94d6d96
Rahelq <belayrahel500@gmail.com>
2024-08-08 14:05:00 +0300

    host modified

--- db/config.js
+++ db/config.js
@@ -4,10 +4,20 @@ const dbConnection = mysql2.createPool({
 	port:process.env.PORT,
 	user: process.env.USER,
 	database: process.env.DATABASE,
-	host: "localhost",
+	host: process.env.HOST,
 	password: process.env.PASSWORD,
 	connectionLimit: 10
 });
+
+
+// console.log("Connecting to database with the following settings:");
+// console.log({
+//   user: process.env.USER,
+//   database: process.env.DATABASE,
+//   host: process.env.HOST,
+//   password: process.env.PASSWORD,
+// });
+
  
 
 // dbconnection.execute("select 'test'", (err, result) => {
